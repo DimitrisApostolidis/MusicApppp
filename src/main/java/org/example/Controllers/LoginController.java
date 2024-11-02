d4661fb14492620abdfcb4958de104da4e24b499
mpantiri <mpantiri@gmail.com>
2024-11-02 16:26:35 +0200

    Version 1.3

--- src/main/java/org/example/Controllers/LoginController.java
+++ src/main/java/org/example/Controllers/LoginController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.example.Models.Model;
+import org.example.DataBaseConnection; // Βεβαιωθείτε ότι αυτή η κλάση υπάρχει
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,5 +19,18 @@ public class LoginController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    login_btn.setOnAction(actionEvent -> Model.getInstance().getViewFactory().showClientWindow());    }
+        login_btn.setOnAction(actionEvent -> handleLogin());
+    }
+
+    private void handleLogin() {
+        String username = username_fld.getText();
+        String password = password_fld.getText();
+
+        DataBaseConnection db = new DataBaseConnection();
+        if (db.verifyCredentials(username, password)) {
+            Model.getInstance().getViewFactory().showClientWindow();
+        } else {
+            error_lbl.setText("Invalid username or password");
+        }
+    }
 }
--- src/main/java/org/example/DataBaseConnection.java
+++ src/main/java/org/example/DataBaseConnection.java
@@ -1,4 +1,4 @@
-package org.example.DataBase;
+package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
